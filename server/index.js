928c4803d504a2780803a1aff062b2e72fe243df
gokulpv <gokulsreenilayam@gmail.com>
2021-10-02 23:16:28 -0700

    final considerations

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ var cors = require("cors");
 require("dotenv").config();
 
 const client = new Twitter({
-  consumer_key: "OunkzQLUzkvDjNjIBOg9UYvhx",
-  consumer_secret: "X1OTApWfhe4Ht9XM9Q3YHwGCgoVdb3xSwzkNmiy8XUeBs2rY6y",
-  access_token_key: "1443084453836775429-YUEVeXF20kphD0tQ4vcad2LfSYT6Xs",
-  access_token_secret: "KEg1NA9sNbtEhQxVzfg96LMscOcDNiygMNLJWWuCsxrXS",
+  consumer_key: "",
+  consumer_secret: "",
+  access_token_key: "",
+  access_token_secret: "",
 });
 
 const defaults = {
