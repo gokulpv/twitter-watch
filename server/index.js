5a8ed480ef1e2f79d1079e5847014ad84a101de0
gokulpv <gokulsreenilayam@gmail.com>
2021-10-02 23:13:23 -0700

    removed keys

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ var cors = require("cors");
 require("dotenv").config();
 
 const client = new Twitter({
-  consumer_key: process.env.consumer_key,
-  consumer_secret: process.env.consumer_secret,
-  access_token_key: process.env.access_token_key,
-  access_token_secret: process.env.access_token_secret,
+  consumer_key: "OunkzQLUzkvDjNjIBOg9UYvhx",
+  consumer_secret: "X1OTApWfhe4Ht9XM9Q3YHwGCgoVdb3xSwzkNmiy8XUeBs2rY6y",
+  access_token_key: "1443084453836775429-YUEVeXF20kphD0tQ4vcad2LfSYT6Xs",
+  access_token_secret: "KEg1NA9sNbtEhQxVzfg96LMscOcDNiygMNLJWWuCsxrXS",
 });
 
 const defaults = {
